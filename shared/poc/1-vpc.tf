815ea417fe5ed7dc790a90f77e858d71c5d4ba6e
Dimitar Ivanov <d.ivanov@mmstat.org>
2024-03-19 17:43:59 +0200

    init tf

---
+++ shared/poc/1-vpc.tf
@@ -0,0 +1,29 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.6.0"
+
+  name = "main"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.10.0/24", "10.0.20.0/24"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Environment = "staging"
+  }
+}
