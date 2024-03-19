eab65b21731f7e897737cacc7942d4f64a44e8ea
Dimitar Ivanov <d.ivanov@mmstat.org>
2024-03-19 20:31:00 +0200

    add eks

--- shared/poc/providers.tf
+++ shared/poc/providers.tf
@@ -7,3 +7,9 @@ provider "aws" {
     }
   }
 }
+
+provider "kubernetes" {
+    host = data.aws_eks_cluster.cluster.endpoint
+    token = data.aws_eks_cluster_auth.cluster.token
+    cluster_ca_certificate = base64encode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+}
